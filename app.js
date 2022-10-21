2fbf2a5472524252ffbf444889971b0ebae1fe9b
jethronbwira@gmail.com <jethronbwira@gmail.com>
2022-10-21 17:28:55 +0200

    feat: refactor pages

--- app.js
+++ app.js
@@ -1,56 +1,45 @@
 const express = require("express")
 const app = express()
-const port = 2101
+const port = 2110
 
 // set the view engine to pug
 
 app.set('view engine', 'pug');
 app.set('views', 'vues');
 
+//routes
 
 app.get('/', (req, res) => {
-
-
-    // res.sendFile(__dirname + "/vues/accueil.pug")
     res.render('accueil')
-
+    
 })
 
-app.get('/about', (req, res) => {
-
-
+app.get('/about', (req, res, next) => {
     res.render('about')
-
-
+   
 })
-
 
 app.get('/mentors', (req, res) => {
-
-
     res.render('mentors')
-
+    
 
 })
 
 app.get('/adresse', (req, res) => {
- 
-
     res.render('adresse')
-
+    console.log("adresse")
 
 })
 
 app.get('/contact', (req, res) => {
 
-
     res.render('contact')
-
 
 })
 
-
+// express listen method
 
 app.listen(port, () => {
     console.log(`mon serveur est écouté sur le port ${port}`)
+
 })
